a6e665129d857810fdc478b861da9ee96a7f81ce
CyberJindu <chrisjindu04@gmail.com>
2025-09-18 16:34:29 -0700

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
   vendorId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   name: { type: String, required: true },
+  category: { type: String, required: true },
   description: String,
   price: { type: Number, required: true },
   stock: { type: Number, default: 0 },
@@ -10,3 +11,4 @@ const productSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("Product", productSchema);
+
