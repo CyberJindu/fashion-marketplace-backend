4a5b7c8f6a906ca635a7b772d80b3050cdffd772
CyberJindu <chrisjindu04@gmail.com>
2025-09-19 07:21:33 -0700

    Update Review.js

--- models/Review.js
+++ models/Review.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 
-const reviewSchema = new mongoose.Schema({
+const reviewSchema = new mongoose.Schema(
+  {
   productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
   orderId: { type: mongoose.Schema.Types.ObjectId, ref: "Order", required: true },
   customerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   rating: { type: Number, min: 1, max: 5 },
   comment: String,
-});
+  },
+  { timestamps: true } 
+);
 
 module.exports = mongoose.model("Review", reviewSchema);
+
