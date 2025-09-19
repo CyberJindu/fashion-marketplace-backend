33e9e4c91a91d81e97c1836eb66dcf0642726f9b
CyberJindu <chrisjindu04@gmail.com>
2025-09-19 07:14:53 -0700

    Update reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Review = require("../models/Review");
 const Order = require("../models/Order");
+const Product = require("../models/Product"); // 
 const { auth } = require("../middleware/auth");
 
 const router = express.Router();
@@ -15,7 +16,13 @@ router.post("/", auth, async (req, res, next) => {
     if (order.orderStatus !== "Delivered")
       return res.status(400).json({ message: "Only after delivery" });
 
-    const review = new Review({ productId, orderId, customerId: req.user._id, rating, comment });
+    const review = new Review({
+      productId,
+      orderId,
+      customerId: req.user._id,
+      rating,
+      comment,
+    });
     await review.save();
     res.status(201).json(review);
   } catch (err) {
@@ -26,7 +33,32 @@ router.post("/", auth, async (req, res, next) => {
 // product reviews
 router.get("/product/:id", async (req, res, next) => {
   try {
-    const reviews = await Review.find({ productId: req.params.id }).populate("customerId", "name");
+    const reviews = await Review.find({ productId: req.params.id }).populate(
+      "customerId",
+      "name"
+    );
+    res.json(reviews);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// vendor reviews
+router.get("/vendor/:vendorId", async (req, res, next) => {
+  try {
+    const { vendorId } = req.params;
+
+    // Find all products owned by this vendor
+    const products = await Product.find({ vendorId }).select("_id name");
+
+    // Extract their IDs
+    const productIds = products.map((p) => p._id);
+
+    // Find reviews for these products
+    const reviews = await Review.find({ productId: { $in: productIds } })
+      .populate("customerId", "name")
+      .populate("productId", "name");
+
     res.json(reviews);
   } catch (err) {
     next(err);
